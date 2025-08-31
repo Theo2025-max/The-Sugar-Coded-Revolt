2a88ebc1197456a24512df7843dfd31e8819692b
Theo2025-max <madzunyetheo@gmail.com>
2025-08-31 10:36:19 +0200

    [10:36, 2025/08/31]
    - Added temporary shield UI using two reference images (shield icon and shield bar)
      to test visual alignment with the game's theme.
    - Implemented basic shield functionality in PlayerHealth.cs to integrate with the UI.
    - This is a prototype; images and visuals are placeholders to evaluate appearance and layout.

--- The-Sugar-Coded-Revolt/Assets/Scripts/Player/PlayerHealth.cs
+++ The-Sugar-Coded-Revolt/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,15 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Range(1, 10)]
     [SerializeField] int startingHealth = 5;
     [SerializeField] CinemachineCamera deathVirtualCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
+
 
     int currentHealth;
     int gameOverVirtualCameraPriority = 20;
@@ -13,11 +17,14 @@ public class PlayerHealth : MonoBehaviour
     private void Awake()
     {
         currentHealth = startingHealth;
+        AdjustShieldUI();
     }
+     
+
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-        Debug.Log(amount + "damage taken");
+        AdjustShieldUI();
 
         if (currentHealth <= 0)
         {
@@ -27,5 +34,21 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+
+    void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
+        }
+    }
 }
  
